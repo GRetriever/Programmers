fb8e5899844514af02b20c9d51539bbfb3b87d49
Kwon Gihun <148840826+GRetriever@users.noreply.github.com>
2023-12-02 19:18:32 +0900

    [level 1] Title: 과일 장수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,7 +1,9 @@
 def solution(k, m, score):
-    answer = 0
-    sor = sorted(score)[::-1]
-    for i in range(0,len(score),m):
-        if len(sor[i:i+m]) == m:
-            answer += min(sor[i:i+m])*m
-    return answer
+    # answer = 0
+    # sor = sorted(score)[::-1]
+    # for i in range(0,len(score),m):
+    #     if len(sor[i:i+m]) == m:
+    #         answer += min(sor[i:i+m])*m
+    # return answer
+
+    return sum((sorted(score)[len(score)%m::m])*m)
