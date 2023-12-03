3a5825b2a99f15a44b0c10e59c6a793d79b422e2
Kwon Gihun <148840826+GRetriever@users.noreply.github.com>
2023-12-03 12:12:03 +0900

    [level 1] Title: 자동차 대여 기록에서 장기/단기 대여 구분하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/151138. 자동차 대여 기록에서 장기／단기 대여 구분하기/자동차 대여 기록에서 장기／단기 대여 구분하기.sql
+++ 프로그래머스/1/151138. 자동차 대여 기록에서 장기／단기 대여 구분하기/자동차 대여 기록에서 장기／단기 대여 구분하기.sql
@@ -1,19 +1,10 @@
 -- 코드를 입력하세요
-# SELECT HISTORY_ID,
-#        CAR_ID,
-#        DATE_FORMAT(START_DATE,'%Y-%m-%d') AS START_DATE,
-#        DATE_FORMAT(END_DATE,'%Y-%m-%d') AS END_DATE,
-#        IF(DATEDIFF(END_DATE,START_DATE)>=29,'장기대여','단기대여') RENT_TYPE
-# FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-# WHERE START_DATE LIKE '%2022-09%'
-# ORDER BY 1 DESC
-
-
-SELECT  history_id,
-        car_id,
-        DATE_FORMAT(start_date,"%Y%-%m-%d") as START_DATE, 
-        DATE_FORMAT(end_date,"%Y-%m-%d") as END_DATE,
+SELECT HISTORY_ID,
+        CAR_ID,
+        DATE_FORMAT(START_DATE,'%Y-%m-%d') AS START_DATE,
+        DATE_FORMAT(END_DATE,'%Y-%m-%d') AS END_DATE,
      if(DATEDIFF(end_date,start_date) >= 29, '장기 대여', '단기 대여') RENT_TYPE
 FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-where date_format(start_date,"%Y-%m") LIKE '2022-09' 
-ORDER BY 1 DESC;
+WHERE date_format(start_date,"%Y-%m") LIKE '2022-09'
+ORDER BY 1 DESC
+
