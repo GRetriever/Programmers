5d08a65be48a3712fda11a51c9b532a471d1a150
Kwon Gihun <xzv2221@gmail.com>
2024-01-01 10:56:54 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,13 +1,15 @@
 def solution(n, lost, reserve):
+    answer = n-len(lost)
     s = set(lost) & set(reserve)
+    answer += len(s)
     l = sorted(list(set(lost) - set(s)))
     r = sorted(list(set(reserve) - set(s)))
             
     for i in l:
         if i-1 in r:
             r.remove(i-1)
+            answer += 1
         elif i+1 in r:
             r.remove(i+1)
-        else:
-            n -= 1
-    return n
+            answer += 1
+    return answer
