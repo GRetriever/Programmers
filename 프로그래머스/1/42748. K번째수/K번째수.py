b8a906fb260b9322978816fef28ab000b4ec968e
Kwon Gihun <xzv2221@gmail.com>
2024-02-01 08:26:59 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,7 +1,5 @@
 def solution(array, commands):
     answer = []
-    for command in commands:
-        i,j,k = command
-        sort = sorted(array[i-1:j])
-        answer.append(sort[k-1])
+    for i,j,k in commands:
+        answer.append(sorted(array[i-1:j])[k-1])
     return answer
