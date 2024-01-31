5deff7588b764a550fb20baea6d460204c3fdf81
Kwon Gihun <xzv2221@gmail.com>
2024-02-01 08:22:07 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,7 +1,7 @@
 def solution(food):
     answer = ''
-    for i in range(1,len(food)):
-        fod = str(i)
-        if food[i] > 1:
-            answer += fod * (food[i]//2)
-    return answer + '0' + answer[::-1]
+    for i,num in enumerate(food):
+        if i == 0:
+            continue
+        answer += str(i)*(num//2)
+    return answer +'0' + answer[::-1]
