a74d0afc24b4e182061c70b98c9d87dc7dbafd50
Kwon Gihun <xzv2221@gmail.com>
2024-02-05 08:50:01 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,14 +1,9 @@
 def solution(n, arr1, arr2):
     answer = []
-    all = []
     for i,j in zip(arr1,arr2):
         map = bin(i|j)[2:]
-        if len(map) < len(arr1):
-            map = str(0)*(len(arr1) - len(map)) + map
-        all.append(map)
-    
-    for i in all:
-        i = i.replace('0',' ')
-        i = i.replace('1','#')
-        answer.append(i)
+        if len(map) < n:
+            map = str(0)*(n-len(map)) + map
+        map = map.replace('0',' ').replace('1','#')
+        answer.append(map)
     return answer
