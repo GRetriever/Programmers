6930ce3da965e18874e4ad8a5d4b4d91adb97b9f
Kwon Gihun <xzv2221@gmail.com>
2024-02-07 08:38:06 +0900

    [level 1] Title: 카드 뭉치, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,10 +1,11 @@
 def solution(cards1, cards2, goal):
-    answer = []
-    for word in goal:
-        if len(cards1) > 0 and word == cards1[0]:
-            answer.append(word)
-            cards1.remove(word)
-        elif len(cards2) > 0 and word == cards2[0]:
-            answer.append(word)
-            cards2.remove(word)
-    return 'Yes' if answer == goal else 'No'
+    deck1 = 0
+    deck2 = 0
+    for i in goal:
+        if deck1 < len(cards1) and cards1[deck1] == i:
+            deck1 += 1
+        elif deck2 < len(cards2) and cards2[deck2] == i:
+            deck2 += 1
+        else:
+            return 'No'
+    return 'Yes'
