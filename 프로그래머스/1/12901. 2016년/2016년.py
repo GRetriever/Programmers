28aca824605f26fea8d8b94783c8499654510cbc
Kwon Gihun <xzv2221@gmail.com>
2024-02-08 08:20:46 +0900

    [level 1] Title: 2016년, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.py
+++ 프로그래머스/1/12901. 2016년/2016년.py
@@ -1,8 +1,5 @@
 def solution(a, b):
     months = [31,29,31,30,31,30,31,31,30,31,30,31]
-    days = ['FRI','SAT','SUN','MON','TUE','WED','THU']
-    cnt = 0
-    cnt += sum(months[:a-1])
-    cnt += b-1
-    date = days[cnt%7]
-    return date
+    date = ['THU','FRI','SAT','SUN','MON','TUE','WED']
+    answer = date[(b+sum(months[:a-1]))%7]
+    return answer
