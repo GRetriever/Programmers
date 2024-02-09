d059248f9fe7bb9f52e0d1c03a36742f81b923c5
Kwon Gihun <xzv2221@gmail.com>
2024-02-09 11:45:24 +0900

    [level 1] Title: 기사단원의 무기, Time: 1.41 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,13 +1,15 @@
 def solution(number, limit, power):
-    answer = 0
+    answer = []
+    
     for i in range(1,number+1):
-        div = 0
-        for j in range(1,int(i**0.5)+1):
+        num = 0
+        for j in range(1,int(i**0.5) + 1):
             if i % j == 0:
-                div += 2
-        if i**0.5 % 1 == 0:
-            div -= 1
-        if div > limit:
-            div = power
-        answer += div
-    return answer
+                num += 1
+                if j*j != i:
+                    num += 1
+        if num > limit:
+            answer.append(power)
+        else:
+            answer.append(num)
+    return sum(answer)
