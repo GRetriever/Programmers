dd848636f0f3b80db92cc0838e64c6eb30fbe890
Kwon Gihun <xzv2221@gmail.com>
2024-02-10 11:15:41 +0900

    [level 1] Title: 소수 찾기, Time: 347.32 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,17 +1,6 @@
 def solution(n):
-    # answer = 0
-    # for i in range(2,n+1):
-    #     prime = 0
-    #     for j in range(2,i):
-    #         if i % j == 0:
-    #             prime += 1
-    #             break
-    #     if prime == 0:
-    #         answer += 1
-    # return answer
-    num = set(range(2,n+1))
-    
+    s = set(range(2,n+1))
     for i in range(2,n+1):
-        if i in num:
-            num -= set(range(2*i,n+1,i))
-    return len(num)
+        if i in s:
+            s -= set(range(2*i,n+1,i))
+    return len(s)
