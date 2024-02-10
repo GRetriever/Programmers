9d2043c5dc780039715f04bd89bfbbe30c10a401
Kwon Gihun <xzv2221@gmail.com>
2024-02-10 11:06:52 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -1,13 +1,13 @@
 def solution(nums):
     from itertools import combinations
-    
     answer = 0
     for i in combinations(nums,3):
-        s = 0
-        for j in range(2,sum(i)):
-            if sum(i) % j == 0:
-                s += 1
-        if s == 0:
-            answer += 1             
+        s = sum(i)
+        div = 0
+        for j in range(2,s):
+            if s % j == 0:
+                div += 1
+                break
+        if div == 0:
+            answer += 1
     return answer
-
