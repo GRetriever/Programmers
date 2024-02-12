c52765e9116fdf0c80283513496a4d62acd20d40
Kwon Gihun <xzv2221@gmail.com>
2024-02-12 12:03:11 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,10 +1,11 @@
 def solution(N, stages):
     dict = {}
-    num = len(stages)
+    l = len(stages)
     for i in range(1,N+1):
         if stages.count(i) != 0:
-            dict[i] = stages.count(i) / num
-            num -= stages.count(i)
+            dict[i] = stages.count(i) / l
+            l -= stages.count(i)
         else:
             dict[i] = 0
-    return sorted(dict.keys(), key=lambda x: dict[x], reverse=True)
+    return sorted(dict, key=lambda x: dict[x], reverse = True )
+    
