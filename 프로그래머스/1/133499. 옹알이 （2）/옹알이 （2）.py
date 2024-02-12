291c0dd4ec6f12daddcd02bae459efd11fe012dd
Kwon Gihun <xzv2221@gmail.com>
2024-02-13 08:38:22 +0900

    [level 1] Title: 옹알이 (2), Time: 0.08 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,13 +1,11 @@
 def solution(babbling):
-    pron = ['aya','ye','woo','ma']
     answer = 0
-    for i in range(len(babbling)):
-        for j in pron:
-            if j*2 in babbling[i]:
-                pass
-            elif j in babbling[i]:
-                babbling[i] = babbling[i].replace(j,'*')
-        if all(char == '*' for char in babbling[i]):
+    words = ["aya", "ye", "woo", "ma"]
+    
+    for bab in babbling:
+        for word in words:
+            if word*2 not in bab:
+                bab = bab.replace(word,' ')
+        if len(bab.strip()) == 0:
             answer += 1
     return answer
-    
