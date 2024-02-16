bda7ab052a5f794b0ef895567d1f1d6c547270a2
Kwon Gihun <xzv2221@gmail.com>
2024-02-17 08:35:26 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 68.18 ms, Memory: 26.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,6 +1,7 @@
 def solution(participant, completion):
-    from collections import Counter
-    p = Counter(participant)
-    c = Counter(completion)
-    m = p-c
-    return list(m.keys())[0]
+    participant.sort()
+    completion.sort()
+    for a,b in zip(participant,completion):
+        if a != b:
+            return a
+    return participant[-1]
