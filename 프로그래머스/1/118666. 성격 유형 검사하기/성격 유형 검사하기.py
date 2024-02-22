9bd5a478df1f657086ac09283715376c00f14837
Kwon Gihun <xzv2221@gmail.com>
2024-02-23 08:21:58 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.14 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,19 +1,20 @@
 def solution(survey, choices):
     from collections import defaultdict
     answer = ''
+    score = {1:3,2:2,3:1,4:0,5:1,6:2,7:3}
     dict = defaultdict(int)
     char = ['RT','CF','JM','AN']
-    score = {1:3,2:2,3:1,4:0,5:1,6:2,7:3}
+    
     for i,j in zip(survey,choices):
-        if j <= 3:
+        if j < 4:
             dict[i[0]] += score[j]
-        elif j >= 4:
+        if j > 4:
             dict[i[1]] += score[j]
-    for i in char:
-        if dict[i[0]] == dict[i[1]]:
-            answer += sorted(i)[0]
-        elif dict[i[0]] > dict[i[1]]:
-            answer += i[0]
-        elif dict[i[0]] < dict[i[1]]:
-            answer += i[1]
+    
+    for i,j in char:
+        if dict[i] >= dict[j]:
+            answer += i
+        else:
+            answer += j
+            
     return answer
