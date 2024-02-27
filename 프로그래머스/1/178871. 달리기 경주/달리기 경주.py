f1c6a40766ce012d93c8cc9ea032f22866cc31e8
Kwon Gihun <xzv2221@gmail.com>
2024-02-28 07:54:00 +0900

    [level 1] Title: 달리기 경주, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
@@ -1,9 +1,9 @@
 def solution(players, callings):
     ranking = {player : i for i,player in enumerate(players)}
     for i in callings:
-        current = ranking[i]
+        cur = ranking[i]
         ranking[i] -= 1
-        ranking[players[current -1]] += 1
+        ranking[players[cur-1]] += 1
         
-        players[current-1],players[current] = i,players[current-1]
-    return players
+        players[cur-1],players[cur] = i,players[cur-1]
+    return players  
